c364d9fe8be5e8b74fd5d625527ba4bd85c79025
Roger Robertson Jr <rjrjr@pm.me>
2020-04-16 14:35:05 -0700

    garbage, utter garbage

--- bikehub/app/models/ride.rb
+++ bikehub/app/models/ride.rb
@@ -2,4 +2,19 @@ class Ride < ApplicationRecord
     belongs_to :user
     belongs_to :bike
     has_many :comments
+
+    def longest_ride
+        longest = Ride.maximum(:duration).id
+
+        longest_ride = Ride.all.find(longest)
+
+    end
+
+    def fastest_ride
+    
+    end
+
+    def most_rides
+
+    end
 end
